d50cf702749aea94ddf1d061a07ecb5e29d692bd
Matias Benedetto <benedettomatias1991@gmail.com>
2022-09-08 21:41:29 -0300

    desafio Sincronizar counter

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -5,24 +5,20 @@ const ItemCount = ({ stock, initial, onAdd }) => {
   const { add, substract, count } = useCounter(stock, initial);
 
   return (
-    <div className="row">
-      <div className="col-md-4">
+      <div className="col-md-12">
         <div className="card border-secondary" style={{ maxWidth: "20rem" }}>
-          <div className="card-header">Desafio Contador</div>
           <div className="card-body">
-            <h4 className="card-title">Producto</h4>
             <p className="card-text">Cantidad: {count}</p>
             <div className="d-flex justify-content-center">
                 <button className="btn btn-light" onClick={substract}> - </button>
                 <button className="btn btn-dark" onClick={add}> + </button>
             </div>
             <div className="d-flex justify-content-center pt-2">
-                <button className="btn btn-primary" onClick={() => onAdd(count)} disabled={count===0}>Confirmar</button>
+                <button className="btn btn-primary" onClick={() => onAdd(count)} disabled={count===0}>Agregar al Carro</button>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,23 +1,33 @@
 import React from "react";
+import ItemCount from "./../ItemCount/ItemCount";
+import Swal from "sweetalert2";
 
-const ItemDetail = ({ item }) => {
+const ItemDetail = ({ item,  }) => {
+  function onAdd(count) {
+    Swal.fire(`Productos seleccionados: ${item.id}, ${item.title}, ${item.id} y cantidad ${count}`);
+  }
+
   return (
     <div className="container">
       <div className="d-flex justify-content-center">
         <div className="card mb-3">
           <h3 className="card-header">{item.title}</h3>
-          <div className="card-body">
-            <h5 className="card-title">{item.title}</h5>
-            <h6 className="card-subtitle text-muted">{item.title}</h6>
-          </div>
-          <img src={`/images/${item.pictureUrl}`} alt="logo" />
-          <div className="card-body">
-            <p className="card-text">
-              {item.description}
-            </p>
-          </div>
-          <div className="card-body">
-            <p>va los botones de agregar compra.</p>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card-body">
+                <h5 className="card-title">{item.title}</h5>
+                <h6 className="card-subtitle text-muted">{item.title}</h6>
+              </div>
+              <img src={`/images/${item.pictureUrl}`} alt="logo" />
+            </div>
+            <div className="col-md-6">
+              <div className="card-body">
+                <p className="card-text">{item.description}</p>
+              </div>
+              <div className="card-body">
+                <ItemCount stock={item.stock} initial={item.stock > 0 ? 1 : 0} onAdd={onAdd} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
